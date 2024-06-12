6f56ce30237cc4ea198ea7e9e67ce4f31d9d36a2
pungpondinwza <86757253+dlwlilac@users.noreply.github.com>
2024-06-12 17:10:09 +0700

    Show Product

--- src/views/apicon.vue
+++ src/views/apicon.vue
@@ -7,11 +7,15 @@
       <v-row justify="center" align="center">
         <div v-for="(item, index) in apiData" :key="index">
           <v-col cols="12">
-            <v-card width="300">
-              <v-img width="300" :src="item.urlImg"></v-img>
+            <v-card width="450">
+              <v-img width="450" :src="item.urlImg"></v-img>
               <v-card-title align="left" primary-title>
                 {{ item.productName }}
-                <v-card-subtitle>{{ item.detail }} <br> ราคา: {{item.price}} <br> คงเหลือ: {{item.stock}} </v-card-subtitle>
+                <v-card-subtitle>
+                  <b>รายละเอียด:</b> {{ item.detail }} <br />
+                  <b>ราคา:</b> {{ item.price }} <br />
+                  <b>คงเหลือ:</b> {{ item.stock }}
+                </v-card-subtitle>
               </v-card-title>
               <v-card-actions>
                 <v-spacer></v-spacer>
@@ -25,7 +29,7 @@
         </div>
       </v-row>
       <br /><br />
-      <v-btn color="success" @click="getData()">ApiCall</v-btn>
+      <!-- <v-btn color="success" @click="getData()">ApiCall</v-btn> -->
     </div>
   </v-container>
 </template>
@@ -47,9 +51,9 @@ export default {
         });
     },
   },
-  //   mounted(){
-  //     this.getData();
-  //   }
+    mounted(){
+      this.getData();
+    }
 };
 </script>
 
